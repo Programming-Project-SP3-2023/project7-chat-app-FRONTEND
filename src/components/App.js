f34599dd1b8ab8427b5879b0bfc1cbb4e05e859e
Jacopo G <S3876518@student.rmit.edu.au>
2023-09-05 17:05:06 +1000

    Create React project + clean up and folder hierarchy setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Echo</h1>
+        <h2>Chat Project</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
