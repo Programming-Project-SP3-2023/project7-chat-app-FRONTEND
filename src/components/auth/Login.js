d922ead01ca30d421249d1a7b4808786ed01a9a7
Jacopo G <S3876518@student.rmit.edu.au>
2023-09-10 13:18:53 +1000

    Login first draft implemented

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,14 +2,117 @@
  * Login component
  */
 
+import {
+  TextField,
+  InputAdornment,
+  FormControl,
+  IconButton,
+  Button,
+  Link,
+} from "@mui/material";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
+import { setUserSession } from "../../utils/localStorage";
+import { useNavigate } from "react-router-dom";
+
 /**
  * Builds and renders the login component
  * @returns Login component render
  */
 const Login = () => {
+  //Props
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+    console.log("LOGIN HANDLER");
+
+    if (email === "" || password === "") {
+      // Set error message
+      setMessage("Both email and password required. Try again.");
+    } else {
+      //TODO - Attempt login request
+      // for now, store user details in localstorage
+      const user = {
+        email: email,
+        password: password,
+      };
+      // set user data in local storage
+      setUserSession(user);
+      // navigate to dashboard
+      navigate("/dashboard");
+    }
+  };
+
   return (
-    <section className="main-section">
+    <section className="main-section" id="login-screen">
       <h1>Login page</h1>
+      <form onSubmit={loginHandler}>
+        <FormControl id="login-form">
+          <p>Email:</p>
+          <TextField
+            fullWidth
+            variant="outlined"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            id="email"
+            placeholder="Enter your email address"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <ContactMailIcon color="primary" />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <p>Password:</p>
+          <TextField
+            fullWidth
+            variant="outlined"
+            value={password}
+            type={showPassword ? "text" : "password"}
+            onChange={(event) => setPassword(event.target.value)}
+            id="password"
+            placeholder="Enter your password"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockOutlinedIcon color="primary" />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          {/* if the message is defined, show it */}
+          {message && <p className="error-message">{message}</p>}
+          <div id="login-button-div">
+            <Button variant="contained" id="login-btn" type="submit">
+              Login
+            </Button>
+          </div>
+        </FormControl>
+      </form>
+      <span id="login-link">
+        Need to create an account?
+        <Link href="/signup">Register</Link>
+        here
+      </span>
     </section>
   );
 };
--- src/components/profile/Dashboard.js
+++ src/components/profile/Dashboard.js
@@ -2,16 +2,35 @@
  * Dashboard component
  */
 
+import { Link } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { getUser, resetUserSession } from "../../utils/localStorage";
 
 /**
  * Builds and renders the dashboard component
  * @returns Dashboard component render
  */
 const Dashboard = () => {
+  // temporary user for development
+  const user = getUser();
+
+  /**
+   * Log user out
+   */
+  const logout = () => {
+    //Delete data from local storage
+    resetUserSession();
+  };
+
   return (
     <section className="main-section">
       <h1>Dashboard page</h1>
+      <h3>Temporary user testing display:</h3>
+      <p>EMAIL: {user && user.email}</p>
+      <p>PASSWORD: {user && user.password}</p>
+      <Link href="/" onClick={logout}>
+        Logout
+      </Link>
       <Outlet />
     </section>
   );
