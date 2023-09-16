768f68512f293d1a37d92b1abf6bc2320798923c
NJoiner <70944219+NJoiner@users.noreply.github.com>
2023-09-16 18:39:51 +1000

    Chat-UI in progress
    Chat UI in progress
    
    temporary visible from dashboard

--- src/components/profile/Friends.js
+++ src/components/profile/Friends.js
@@ -2,15 +2,18 @@
  * Friends Chats component
  */
 
+import { Box } from "@mui/material";
+
 /**
  * Builds and renders the friends chats component
  * @returns Friends chats component render
  */
-const Friends = () => {
+
+const Friends = ({ friends_list, setFriendsOpt, selectedFriend }) => {
   return (
-    <section className="main-section">
-      <h1>Friends Chats</h1>
-    </section>
+    <Box sx={{ overflow: "auto", width: "100%" }}>
+      <p>friends List</p>
+    </Box>
   );
 };
 
