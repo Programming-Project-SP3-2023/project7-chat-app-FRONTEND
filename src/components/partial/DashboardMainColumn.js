bbcd76449ffacc141b538cb18fd27866b57ff51e
Jacopo G <S3876518@student.rmit.edu.au>
2023-09-19 14:22:11 +0200

    Menu items

--- src/components/partial/DashboardMainColumn.js
+++ src/components/partial/DashboardMainColumn.js
@@ -2,6 +2,8 @@
  * Dashboard Main Column component
  */
 
+import MenuItem from "./MenuItem";
+
 /**
  * Builds and renders the Dashboard Main Column component
  * @returns Dashboard Main Column component render
@@ -12,6 +14,9 @@ const DashboardMainColumn = ({title}) => {
     <div className="dashboard-main-column">
       <div className="column-header">
         <h3>{title}</h3>
+      </div>
+      <div className="column-options">
+      <MenuItem />
       </div>
     </div>
   );
