7db0020f1ce920699cb48391a5bdf0e3457b96a9
NJoiner <70944219+NJoiner@users.noreply.github.com>
2023-09-28 21:31:18 +1000

    QuickActions / EditProfile/ Textfield btns
    Implementation of show/hide submit buttons for text fields + additional field disable function

--- src/components/profile/EditProfile.js
+++ src/components/profile/EditProfile.js
@@ -1,17 +1,15 @@
-import { TextField, Avatar, Box } from "@mui/material";
+import { TextField, Avatar, Box, Icon, useMediaQuery } from "@mui/material";
 import { Modal } from "@mui/material";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-
-// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import IconButton from "@mui/material/IconButton";
-
-import EditIcon from "@mui/icons-material/Edit";
-
 import { getUser } from "../../utils/localStorage";
-
 import { useState } from "react";
 
+import ButtonGroup from "@mui/material/ButtonGroup";
+
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
 import PasswordUpdateModal from "./PasswordUpdate";
+import DoneIcon from "@mui/icons-material/Done";
 
 const EditProfile = ({ editProfileModalOpen, setEditProfileModalOpen }) => {
   const user = getUser();
@@ -20,19 +18,43 @@ const EditProfile = ({ editProfileModalOpen, setEditProfileModalOpen }) => {
   const handleClose = () => {
     setEditProfileModalOpen(false);
   };
+  // by setting useState as(true) each text field is disabled initially
+  const [isNameDisabled, setIsNameDisabled] = useState(true);
+  const [isEmailDisabled, setIsEmailDisabled] = useState(true);
+  const [isDateOfBirthDisabled, setIsDateOfBirthDisabled] = useState(true);
+
+  const [isNameSubmitVisible, setIsNameSubmitVisble] = useState(false);
+  const [isEmailSubmitVisible, setIsEmailSubmitVisible] = useState(false);
+  const [isDateOfBirthSubmitVisible, setIsDateOfBirthVisible] = useState(false);
 
   const [selectedImage, setSelectedImage] = useState(null);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [dateOfBirth, setDateOfBirth] = useState("");
 
+  // handle disabling and unlocking text fields
+  const handleNameDisabled = () => {
+    setIsNameSubmitVisble(!isNameSubmitVisible);
+    setIsNameDisabled(!isNameDisabled);
+  };
+
+  const handleEmailDisabled = () => {
+    setIsEmailSubmitVisible(!isEmailSubmitVisible);
+    setIsEmailDisabled(!isEmailDisabled);
+  };
+
+  const handleDateOfBirthDisabled = () => {
+    setIsDateOfBirthVisible(!isDateOfBirthSubmitVisible);
+    setIsDateOfBirthDisabled(!isDateOfBirthDisabled);
+  };
+
   // Handles image upload and formats it to Base64
   const imageChange = (e) => {
     if (e.target.files && e.target.files.length > 0) {
       const data = new FileReader();
-      data.addEventListener('load', () => {
+      data.addEventListener("load", () => {
         setSelectedImage(data.result);
-      })
+      });
       data.readAsDataURL(e.target.files[0]);
     }
   };
@@ -79,14 +101,23 @@ const EditProfile = ({ editProfileModalOpen, setEditProfileModalOpen }) => {
                 id="name"
                 variant="outlined"
                 value={name}
+                input
+                disabled={isNameDisabled}
                 onChange={(event) => setName(event.target.value)}
                 type="text"
                 placeholder={user.name}
                 InputProps={{
                   endAdornment: (
-                    <IconButton position="end">
-                      <EditIcon color="primary" />
-                    </IconButton>
+                    <ButtonGroup position="end">
+                      {isNameSubmitVisible ? (
+                        <IconButton>
+                          <DoneIcon />
+                        </IconButton>
+                      ) : null}
+                      <IconButton onClick={handleNameDisabled}>
+                        <EditIcon color="primary" />
+                      </IconButton>
+                    </ButtonGroup>
                   ),
                 }}
               />
@@ -97,14 +128,23 @@ const EditProfile = ({ editProfileModalOpen, setEditProfileModalOpen }) => {
                 id="email"
                 variant="outlined"
                 value={email}
+                input
+                disabled={isEmailDisabled}
                 onChange={(event) => setEmail(event.target.value)}
                 type="email"
                 placeholder={user.email}
                 InputProps={{
                   endAdornment: (
-                    <IconButton position="end">
-                      <EditIcon color="primary" />
-                    </IconButton>
+                    <ButtonGroup position="end">
+                      {isEmailSubmitVisible ? (
+                        <IconButton>
+                          <DoneIcon />
+                        </IconButton>
+                      ) : null}
+                      <IconButton onClick={handleEmailDisabled}>
+                        <EditIcon color="primary" />
+                      </IconButton>
+                    </ButtonGroup>
                   ),
                 }}
               />
@@ -115,14 +155,23 @@ const EditProfile = ({ editProfileModalOpen, setEditProfileModalOpen }) => {
                 id="dateOfBirth"
                 variant="outlined"
                 value={dateOfBirth}
+                input
+                disabled={isDateOfBirthDisabled}
                 onChange={(event) => setDateOfBirth(event.target.value)}
                 type="date"
                 placeholder={user.dateOfBirth}
                 InputProps={{
                   endAdornment: (
-                    <IconButton position="end">
-                      <EditIcon color="primary" />
-                    </IconButton>
+                    <ButtonGroup position="end">
+                      {isDateOfBirthSubmitVisible ? (
+                        <IconButton>
+                          <DoneIcon />
+                        </IconButton>
+                      ) : null}
+                      <IconButton onClick={handleDateOfBirthDisabled}>
+                        <EditIcon color="primary" />
+                      </IconButton>
+                    </ButtonGroup>
                   ),
                 }}
               />
