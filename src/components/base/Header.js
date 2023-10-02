4cad478855e3391222c2642360dce0786c28503e
Jacopo G <S3876518@student.rmit.edu.au>
2023-10-02 18:51:57 +0200

    Dropdown menu button

--- src/components/base/Header.js
+++ src/components/base/Header.js
@@ -5,26 +5,32 @@
 import { useEffect, useState } from "react";
 import ECHO_LOGO from "../../assets/echo_transparent.png";
 import { Link } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 /**
  * Builds and renders the header component
  * @returns Header component render
  */
-const Header = ({isLoggedIn}) => {
-
+const Header = ({ isLoggedIn }) => {
   const [headerId, setHeaderId] = useState("header");
+
+  // dropdown menu functions
+  const openDropdown = () => {
+    console.log("open dropdown");
+  }
 
   useEffect(() => {
     setHeaderId(isLoggedIn ? "header-dark" : "header");
-  }, [isLoggedIn])
+  }, [isLoggedIn]);
 
   return (
     <header id={headerId}>
       <div id="logo-container">
         <Link href="/">
-        <img src={ECHO_LOGO} alt="Echo - a professional chat tool" />
+          <img src={ECHO_LOGO} alt="Echo - a professional chat tool" />
         </Link>
       </div>
+      {headerId === "header-dark" && <MenuIcon id="dropdown-menu-icon" onClick={openDropdown}/>}
     </header>
   );
 };
