eb764043102822cccda2c9e99daae5ce5da9970b
NJoiner <70944219+NJoiner@users.noreply.github.com>
2023-10-11 16:23:49 +1100

    small fix

--- src/components/profile/EditProfile.js
+++ src/components/profile/EditProfile.js
@@ -89,6 +89,7 @@ const EditProfile = ({ editProfileModalOpen, setEditProfileModalOpen }) => {
     if (email === "") {
       setMessage("New Email cannot be empty");
     } else {
+      user.email = email;
       setIsEmailDisabled(!isEmailDisabled);
       setIsEmailSubmitVisible(!isEmailSubmitVisible);
       setMessage("email: " + user.email + " has been updated");
